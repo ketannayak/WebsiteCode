f60dd7d2d8265165516890b356a38e7578bfa67d
Ketan N <ketan.nayak@gmail.com>
2015-02-10 08:25:34 -0500

    Latest changes to website

---
+++ js/projectscript.js
@@ -0,0 +1,20 @@
+$(document).ready(function(){
+	$('.projectone').on('click', function(){
+		$('.p1').slideToggle("slow");
+	});
+	$('.projecttwo').on('click', function(){
+		$('.p2').slideToggle("slow");
+	});
+	$('.projectthree').on('click', function(){
+		$('.p3').slideToggle("slow");
+	});
+	$('.projectfour').on('click', function(){
+		$('.p4').slideToggle("slow");
+	});
+	$('.projectfive').on('click', function(){
+		$('.p5').slideToggle("slow");
+	});
+	$('.projectsix').on('click', function(){
+		$('.p6').slideToggle("slow");
+	});
+});
---
+++ js/script.js
@@ -0,0 +1,35 @@
+$(document).ready(function(){
+	$('.leadershipone').on('click', function(){
+		$('.l1').slideToggle("slow");
+	});
+	$('.leadershiptwo').on('click', function(){
+		$('.l2').slideToggle("slow");
+	});
+	$('.leadershipthree').on('click', function(){
+		$('.l3').slideToggle("slow");
+	});
+	$('.leadershipfour').on('click', function(){
+		$('.l4').slideToggle("slow");
+	});
+	$('.leadershipfive').on('click', function(){
+		$('.l5').slideToggle("slow");
+	});
+	$('.awardsone').on('click', function(){
+		$('.a1').slideToggle("slow");
+	});
+		$('.awardstwo').on('click', function(){
+		$('.a2').slideToggle("slow");
+	});
+		$('.awardsthree').on('click', function(){
+		$('.a3').slideToggle("slow");
+	});
+		$('.awardsfour').on('click', function(){
+		$('.a4').slideToggle("slow");
+	});
+		$('.awardsfive').on('click', function(){
+		$('.a5').slideToggle("slow");
+	});
+		$('.awardssix').on('click', function(){
+		$('.a5').slideToggle("slow");
+	});
+});
